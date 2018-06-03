422975c0ff93e0ec56eb1d122974e2e1d83ba753
Doc Holliday <doc.holliday@usa.com>
2018-06-03 15:01:44 -0400

    Add initial bits for module show

---
+++ show.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var chalk = require('chalk');
+
+////////////////////
+// chalk styles
+global.bold              = chalk.bold;
+global.dim               = chalk.dim;
+global.italic            = chalk.italic;
+global.underline         = chalk.underline;
+global.inverse           = chalk.inverse;
+global.hidden            = chalk.hidden;
+global.strikethrough     = chalk.strikethrough;
+
+global.black             = chalk.black;
+global.red               = chalk.red;
+global.green             = chalk.green;
+global.yellow            = chalk.yellow;
+global.blue              = chalk.blue;
+global.magenta           = chalk.magenta;
+global.cyan              = chalk.cyan;
+global.white             = chalk.white;
+
+global.gray              = chalk.gray;
+global.bright_red        = chalk.redBright;
+global.bright_green      = chalk.greenBright;
+global.bright_yellow     = chalk.yellowBright;
+global.bright_blue       = chalk.blueBright;
+global.bright_magenta    = chalk.magentaBright;
+global.bright_cyan       = chalk.cyanBright;
+global.bright_white      = chalk.whiteBright;
+
+global.bg_black          = chalk.bgBlack;
+global.bg_red            = chalk.bgRed;
+global.bg_green          = chalk.bgGreen;
+global.bg_yellow         = chalk.bgYellow;
+global.bg_blue           = chalk.bgBlue;
+global.bg_magenta        = chalk.bgMagenta;
+global.bg_cyan           = chalk.bgCyan;
+global.bg_white          = chalk.bgWhite;
+
+global.bg_bright_black   = chalk.bgBlackBright;
+global.bg_bright_red     = chalk.bgRedBright;
+global.bg_bright_green   = chalk.bgGreenBright;
+global.bg_bright_yellow  = chalk.bgYellowBright;
+global.bg_bright_blue    = chalk.bgBlueBright;
+global.bg_bright_magenta = chalk.bgMagentaBright;
+global.bg_bright_cyan    = chalk.bgCyanBright;
+global.bg_bright_white   = chalk.bgWhiteBright;
