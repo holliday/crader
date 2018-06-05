a8e0352be6593a8347132ac68c44037d06b02464
Doc Holliday <doc.holliday@usa.com>
2018-06-05 11:09:57 -0400

    Add strat template conf

---
+++ conf/strat.js
@@ -0,0 +1,21 @@
+var conf = {};
+
+// strat name
+conf.strat = '';
+
+// strat data
+//
+// For candlestick strats, 'count' should be set to number
+// of candles and 'frame' should be set to candle size.
+//
+// Otherwise, 'count' should be set to 1 and 'frame' should
+// be set to needed length of time for past trades.
+//
+conf.frame = '1m';
+conf.count = 60;
+
+//
+// other strat-related settings
+//
+
+module.exports = conf;
