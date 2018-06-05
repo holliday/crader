74255ea5b8ec2c13a2f7baeb63a87d55298672c0
Doc Holliday <doc.holliday@usa.com>
2018-06-05 17:24:51 -0400

    FeedBase: use underscore

--- feed/base.js
+++ feed/base.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('underscore');
 const EventEmitter = require('events');
 
 class FeedBase extends EventEmitter {
@@ -22,7 +23,7 @@ class FeedBase extends EventEmitter {
     // If 'this.end' is undefined, run in live mode until stopped.
     //
     async run() {
-        for(var now = typeof this.start === 'undefined' ? Date.now() : this.start;
+        for(var now = _.isUndefined(this.start) ? Date.now() : this.start;
             !(now > this.end);
             now = Math.min(now + this.step, Date.now())
         ) this.emit('trades', await this.fetch_trades(now - this.length, now));
