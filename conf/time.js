91599b1eb6799c3deb7f4c61bca812a5f45de261
Doc Holliday <doc.holliday@usa.com>
2018-06-05 11:10:20 -0400

    Add time template conf

---
+++ conf/time.js
@@ -0,0 +1,24 @@
+var conf = {};
+
+// start date
+//conf.start = '2018-01-01 00:00';
+
+//
+// end date
+// if not specified, will continue running in live mode
+//
+//conf.end = '2018-01-08 00:00';
+
+//
+// alternative way of specifying start date relative
+// to the end date (or present time, if no end date)
+//
+// If both 'start' and 'period' were specified, whichever
+// was specified last will be in effect.
+//
+//conf.period =  '4w'; // 4 weeks
+//conf.period =  '7d'; // 7 days
+//conf.period = '24h'; // 24 hours
+//conf.period = '60m'; // 60 minutes
+
+module.exports = conf;
