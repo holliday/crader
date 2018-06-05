a129370c3ec5274140a1739f07b5c29321a84e56
Doc Holliday <doc.holliday@usa.com>
2018-06-05 17:22:47 -0400

    StratBase: initial implementation

--- strat/base.js
+++ strat/base.js
@@ -1,18 +1,31 @@
 'use strict';
 
+const _ = require('underscore');
 const EventEmitter = require('events');
+
+const advice = require('../advice');
+require('../show');
 
 class StratBase extends EventEmitter {
     constructor(conf) {
         super();
-    }
 
-    init() {
-        // do something
+        if(_.isUndefined(conf.strat) || conf.strat === '')
+            throw new Error('Unspecified or invalid strat');
+
+        console.log('Creating strat:', bold(conf.strat));
+        this.strat = require('./' + conf.strat);
+
+        console.log('Initializing strat');
+        this.strat.init(conf);
     }
 
     advise(trades) {
-        // do something
+        if(_.isArray(trades)) {
+            var adv = this.strat.advise(trades);
+            if(advice.is_buy(adv) || advice.is_sell(adv)) this.emit('advice', adv);
+
+        } else console.warn('Ignoring invalid trades');
     }
 };
 
