3b850bdae1fb978b80ae45192d4d3fac73ec54a0
Doc Holliday <doc.holliday@usa.com>
2018-06-05 22:57:04 -0400

    advice: simplify

--- advice.js
+++ advice.js
@@ -6,29 +6,29 @@ const buy_type = {};
 const sell_type = {};
 
 class Advice {
-    constructor(timestamp, price) {
+    constructor(timestamp, price, type) {
         this.timestamp = timestamp;
         this.price = price;
+        this.type = type;
     }
-    type() { return undefined; }
 }
 
 class BuyAdvice extends Advice {
-    constructor(timestamp, price) { super(timestamp, price) }
-    type() { return buy_type; }
+    constructor(timestamp, price) {
+        super(timestamp, price, buy_type);
+    }
 }
 
 class SellAdvice extends Advice {
-    constructor(timestamp, price) { super(timestamp, price) }
-    type() { return sell_type; }
+    constructor(timestamp, price) {
+        super(timestamp, price, sell_type);
+    }
 }
 
-advice.none = () => new Advice();
-
 advice.buy = (timestamp, price) => new BuyAdvice(timestamp, price);
-advice.is_buy = advice => advice instanceof Advice && advice.type() === buy_type;
+advice.is_buy = advice => advice instanceof Advice && advice.type === buy_type;
 
 advice.sell = (timestamp, price) => new SellAdvice(timestamp, price);
-advice.is_sell = advice => advice instanceof Advice && advice.type() === sell_type;
+advice.is_sell = advice => advice instanceof Advice && advice.type === sell_type;
 
 module.exports = advice;
