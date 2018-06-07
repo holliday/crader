c101ae49e4096ba79f384a2354baee86075f670e
Doc Holliday <doc.holliday@usa.com>
2018-06-06 22:25:07 -0400

    strat/noop: use as_...() functions

--- strat/noop.js
+++ strat/noop.js
@@ -16,7 +16,7 @@ strat.advise = trades => {
     console.log('Received', bold(trades.length), 'trades:');
     trades.forEach(trade => {
         console.log(blue(as_date(trade.timestamp)),
-            cyan(fmt('%9.8g', trade.quantity)), '@', yellow(trade.price)
+            yellow(as_vol(trade.quantity)), '@', bold(as_price(trade.price, '-'))
         );
     });
 
