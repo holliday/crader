2ea08e7193cdabcfc08514da01301023bed3896d
Doc Holliday <doc.holliday@usa.com>
2018-06-08 00:07:01 -0400

    Move stuff from LiveFeed to FeedBase

--- feed/base.js
+++ feed/base.js
@@ -3,31 +3,75 @@
 const _ = require('underscore');
 const EventEmitter = require('events');
 
+const common = root_require('common');
+root_require('show');
+
+////////////////////
 class FeedBase extends EventEmitter {
-    constructor(start, end, step, length) {
+    constructor(conf) {
         super();
 
-        this.start = start;
-        this.end = end;
-        this.step = step;
-        this.length = length;
+        this.exchange = common.parse_text(conf, 'exchange');
+        console.log('Exchange:', bold(this.exchange));
+
+        this.asset = common.parse_text(conf, 'asset');
+        this.currency = common.parse_text(conf, 'currency');
+        this.symbol = this.asset + '/' + this.currency;
+        console.log('Symbol:', bold(this.symbol));
+
+        this.frame = common.parse_period(conf, 'frame');
+        this.count = common.parse_int(conf, 'count');
+        console.log('Length:', bold(this.frame), 'x', bold(this.count));
+
+        this.end = undefined;
+        if(!_.isUndefined(conf.end)) this.end = common.parse_date(conf, 'end');
+
+        this.start = undefined;
+        for(var name in conf)
+            switch(name) {
+                case 'start':
+                    this.start = common.parse_date(conf, name);
+                    break;
+
+                case 'period':
+                    this.start = (_.isUndefined(this.end) ?  Date.now() : this.end) -
+                        common.parse_period(conf, 'period');
+                    break;
+            }
+
+        if(!_.isUndefined(this.end) && _.isUndefined(this.start))
+            throw new Error('Missing one of start or period');
+
+        if(!_.isUndefined(this.start))
+            console.log('Interval:',
+                bold(as_date(new Date(this.start))),
+                'to', 
+                bold(_.isUndefined(this.end) ? '...' : as_date(new Date(this.end)))
+            );
+
+        this.step = 1000;
     }
 
+    ////////////////////
     async fetch_trades(from, to) {
         return [];
     }
 
+    ////////////////////
     //
-    // Run from 'this.start' to 'this.end' timestamp with 'this.step' interval.
-    // If 'this.start' is undefined, start in live mode.
-    // If 'this.end' is undefined, run in live mode until stopped.
+    // Run from 'start' to 'end' timestamp with 'step' interval.
+    // If 'start' is undefined, start in live mode.
+    // If 'end' is undefined, run in live mode until stopped.
     //
     async run() {
         for(var now = _.isUndefined(this.start) ? Date.now() : this.start;
             !(now > this.end);
             now = Math.min(now + this.step, Date.now())
-        ) this.emit('trades', await this.fetch_trades(now - this.length, now));
+        ) this.emit('trades',
+            await this.fetch_trades(now - this.frame * this.count, now)
+        );
     }
 };
 
+////////////////////
 module.exports = FeedBase;
--- feed/live.js
+++ feed/live.js
@@ -3,84 +3,30 @@
 const _ = require('underscore');
 const ccxt = require('ccxt');
 
-const common = root_require('common');
 const FeedBase = root_require('feed/base');
 root_require('show');
 
 ////////////////////
 class LiveFeed extends FeedBase {
-    constructor(exchange, symbol, start, end, length) {
-        super(start, end, exchange.rateLimit, length);
+    constructor(conf) {
+        super(conf);
 
-        this.exchange = exchange;
-        this.symbol = symbol;
-
-        this.trades = [];
-    }
-
-    ////////////////////
-    static create(conf) {
         console.log('Creating', bold('live'), 'feed');
 
-        if(!ccxt.exchanges.includes(conf.exchange))
+        if(!ccxt.exchanges.includes(this.exchange))
             throw new Error('Unspecified or invalid exchange');
 
-        console.log('Creating exchange:', bold(conf.exchange));
-        var exchange = new ccxt[conf.exchange]();
+        console.log('Creating exchange');
+        this.exch = new ccxt[this.exchange]();
+        this.exch.apiKey = conf.api_key;
+        this.exch.secret = conf.secret;
+        this.exch.enableRateLimit = true;
+        this.step = this.exch.rateLimit;
 
-        exchange.apiKey = conf.api_key;
-        exchange.secret = conf.secret;
-        exchange.enableRateLimit = true;
-
-        if(!exchange.hasFetchTrades)
+        if(!this.exch.hasFetchTrades)
             throw new Error('Exchange does not provide trade data');
 
-        var asset = common.parse_text(conf, 'asset');
-        var currency = common.parse_text(conf, 'currency');
-
-        var symbol = asset + '/' + currency;
-        console.log('Symbol:', bold(symbol));
-
-        exchange.loadMarkets().then(() => {
-            if(!exchange.symbols.includes(symbol))
-                throw new Error('Unsupported symbol ' + symbol);
-        });
-
-        var frame = common.parse_period(conf, 'frame');
-        var count = common.parse_int(conf, 'count');
-        console.log('Length:', bold(frame), 'x', bold(count));
-
-        var end;
-        if(!_.isUndefined(conf.end)) {
-            end = (new Date(conf.end)).getTime();
-            if(isNaN(end)) throw new Error('Invalid end time');
-        }
-
-        var start;
-        for(var name in conf)
-            switch(name) {
-                case 'start':
-                    start = (new Date(conf.start)).getTime();
-                    if(isNaN(start)) throw new Error('Invalid start time');
-                    break;
-
-                case 'period':
-                    var period = common.parse_period(conf, 'period');
-                    start = (_.isUndefined(end) ?  Date.now() : end) - period;
-                    break;
-            }
-
-        if(!_.isUndefined(end) && _.isUndefined(start))
-            throw new Error('Missing one of start or period');
-
-        if(!_.isUndefined(start))
-            console.log('Interval:',
-                bold(as_date(new Date(start))),
-                'to', 
-                bold(_.isUndefined(end) ? '...' : as_date(new Date(end)))
-            );
-
-        return new LiveFeed(exchange, symbol, start, end, frame * count);
+        this.trades = [];
     }
 
     ////////////////////
@@ -91,7 +37,7 @@ class LiveFeed extends FeedBase {
 
             var trades;
             for(;;) try {
-                trades = await this.exchange.fetchTrades(this.symbol, since);
+                trades = await this.exch.fetchTrades(this.symbol, since);
                 break;
             } catch (e) {
                 console.error(e);
@@ -118,4 +64,4 @@ class LiveFeed extends FeedBase {
 };
 
 ////////////////////
-module.exports = conf => LiveFeed.create(conf);
+module.exports = conf => new LiveFeed(conf);
