30f81628f6a489597ddba5f80d1227e09cb28885
Doc Holliday <doc.holliday@usa.com>
2018-06-08 11:35:50 -0400

    CacheFeed: read trades in chunks

--- feed/cache.js
+++ feed/cache.js
@@ -16,26 +16,38 @@ class CacheFeed extends FeedBase {
         console.log('Creating', bold('cache'), 'feed');
 
         const name = this.exchange + '_' + this.asset + '_' + this.currency;
-
         console.log('Opening cache database:', bold(name));
-        this.db = new sqlite('cache/' + name + '.sqlite', { readonly: true });
 
-        this.stmt = this.db.prepare(`SELECT timestamp, price, amount
-            FROM data WHERE timestamp >= ? AND timestamp <= ?`
+        this.db = new sqlite('cache/' + name + '.sqlite', { readonly: true });
+        this.fetch = this.db.prepare(`SELECT timestamp, price, amount
+            FROM data WHERE timestamp >= ? LIMIT 1000`
         );
 
         if(ccxt.exchanges.includes(this.exchange)) {
             var exch = new ccxt[this.exchange]();
             this.step = exch.rateLimit;
         }
+
+        this.trades = [];
     }
 
     ////////////////////
     async fetch_trades(from, to) {
-        var trades = this.stmt.all(from, to);
-        if(!trades.length) common.sleep_for(this.step);
+        for(;;) {
+            var since = this.trades.length ? _.last(this.trades).timestamp + 1 : from;
+            if(since > to) break;
 
-        return trades;
+            var trades = this.fetch.all(since);
+            if(!trades.length) {
+                common.sleep_for(this.step);
+                break;
+            }
+
+            this.trades.push(...trades);
+        }
+
+        this.trades = this.trades.filter(trade => trade.timestamp >= from);
+        return this.trades.filter(trade => trade.timestamp <= to);
     }
 };
 
