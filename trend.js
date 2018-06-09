ecbbcce2c38fedac6bead71dedf17289cf5d2ce1
Doc Holliday <doc.holliday@usa.com>
2018-06-08 22:11:35 -0400

    Add class Trend

---
+++ trend.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const _ = require('underscore');
+
+////////////////////
+class Trend {
+    constructor() {
+        this.states = {};
+        this.count = 0;
+    }
+
+    add_state(name, advice, persist) {
+        this.states[name] = { advice: advice, persist: persist };
+    }
+
+    set state(name) {
+        if(name !== this.name) {
+            this.name = name;
+            this.count = 0;
+            this.advised = false;
+        }
+        ++this.count;
+    }
+
+    advise(timestamp, price) {
+        var state = this.states[this.name];
+        if(_.isUndefined(state)
+            || this.advised
+            || this.count <= state.persist
+        ) return;
+
+        var advice = state.advice;
+        if(_.isUndefined(advice)) return;
+
+        this.advised = true;
+        return advice(timestamp, price);
+    }
+}
+
+////////////////////
+module.exports = Trend;
