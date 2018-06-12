bd203bef1bbd9c9fba77af7c3585e9867c591f70
Doc Holliday <doc.holliday@usa.com>
2018-06-12 12:51:53 -0400

    cache: catch up with the all the changes

--- cache.js
+++ cache.js
@@ -1,86 +1,23 @@
 'use strict';
-global.root_require = name => require(__dirname + '/' + name);
 
 const _ = require('underscore');
-require('manakin').global; // color console
-const meow = require('meow');
 
-const common = root_require('common');
+const common = require('./common');
 root_require('show');
 const sqlite = root_require('sqlite3');
 
 ////////////////////
-function read_args() {
-    var args = meow({
-        flags: {
-            help: {
-                type: 'boolean',
-                alias: 'h',
-            },
-            version: {
-                type: 'boolean',
-                alias: 'v',
-            },
-        },
-        help:
-`
-Usage: ${common.node} ${common.name} [option] <conf>...
-
-Where [option] is one of:
-    -h, --help           Show this help screen
-    -v, --version        Show version
-
-and <conf> is one or more conf files to use.
-`
-    });
-    return args;
-}
-
-////////////////////
-function add_to(db, trades) {
-    if(!trades.length) return;
-
-    const stmt = db.prepare(`INSERT OR REPLACE INTO data
-        VALUES (:timestamp, :date, :price, :amount);`
-    );
-
-    db.begin();
-    try {
-        trades.forEach(trade => {
-            Object.assign(trade, { date: as_date(trade.timestamp) });
-            stmt.run(trade);
-        });
-
-        console.log('Cached', as_int(trades.length), 'trades:',
-            blue(as_date(_.first(trades).timestamp)),
-            '-',
-            blue(as_date(_.last(trades).timestamp)),
-        );
-
-        db.commit();
-    } finally {
-        if(db.inTransaction) db.rollback();
-    }
-}
-
-////////////////////
 (async () => { try {
-    common.banner();
+    var conf = { frame: '1m', count: 1 };
 
-    var args = read_args();
-
-    var conf = common.read_conf(args.input);
-    conf.frame = '1m';
-    conf.count = 1;
-    console.log("Merged conf:", conf);
+    common.read_args(conf);
+    common.process(conf);
 
     // set up database
-    const name = common.parse_text(conf, 'exchange')
-         + '_' + common.parse_text(conf, 'asset')
-         + '_' + common.parse_text(conf, 'currency');
+    var db_name = conf.exchange_name + '_' + conf.symbol.replace('/', '_');
+    console.log('Opening cache database:', bold(db_name));
 
-    console.log('Opening cache database:', bold(name));
-    var db = new sqlite('cache/' + name + '.sqlite');
+    var db = new sqlite('cache/' + db_name + '.sqlite');
 
     db.exec(`CREATE TABLE IF NOT EXISTS data (
         timestamp INTEGER PRIMARY KEY,
@@ -89,22 +26,48 @@ function add_to(db, trades) {
         amount REAL NOT NULL
     ) WITHOUT ROWID;`);
 
-    // if neither start nor period was specified,
-    // get last timestamp from the database
-    if(!('start' in conf || 'period' in conf)) {
-        var trades = db.prepare(`SELECT * FROM data
+    var db_insert = db.prepare(`INSERT OR REPLACE INTO data
+        VALUES (:timestamp, :date, :price, :amount);`
+    );
+
+    // if no start, get the last timestamp from the database
+    if(_.isUndefined(conf.start)) {
+        var trade = db.prepare(`SELECT * FROM data
             ORDER BY timestamp DESC LIMIT 1;`
         ).all();
 
-        if(trades.length) conf.start = trades[0].timestamp;
+        if(trade.length) {
+            conf.start = trade[0].timestamp;
+            console.log('Resuming from:', bold(as_date(conf.start)));
+        }
     }
 
     // set up live feed
-    var feed = root_require('feed/live')(conf);
-    feed.step = feed.frame * feed.count;
+    var feed = await common.local_require('feed', 'live').create(conf);
+    conf.step = conf.frame * conf.count;
 
     // rock-n-roll
-    feed.on('trades', trades => add_to(db, trades));
+    feed.on('trades', trades => {
+        if(!trades.length) return;
+
+        db.begin();
+        try {
+            trades.forEach(trade => {
+                Object.assign(trade, { date: as_date(trade.timestamp) });
+                db_insert.run(trade);
+            });
+
+            console.log('Cached', as_int(trades.length), 'trades:',
+                blue(as_date(_.first(trades).timestamp)),
+                '-',
+                blue(as_date(_.last(trades).timestamp)),
+            );
+
+            db.commit();
+        } finally {
+            if(db.inTransaction) db.rollback();
+        }
+    });
     await feed.run();
 
     console.log('DONE!');
