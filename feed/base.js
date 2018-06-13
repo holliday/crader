8f47562132744b512476b18e559fe14cfb5a6b59
Doc Holliday <doc.holliday@usa.com>
2018-06-12 23:15:17 -0400

    FeedBase: store initial and last prices

--- feed/base.js
+++ feed/base.js
@@ -29,11 +29,19 @@ class FeedBase extends EventEmitter {
         for(var now = _.isUndefined(this.conf.start) ? Date.now() : this.conf.start;
             !(now > this.conf.end || this.conf.stop);
             now = Math.min(now + this.conf.step, Date.now())
+        ) {
+            var trades = (await Promise.all([
+                this.fetch_trades(now - this.conf.frame * this.conf.count, now),
+                common.sleep_for(0), // allow Ctrl+C
+            ]))[0];
 
-        ) this.emit('trades', (await Promise.all([
-            this.fetch_trades(now - this.conf.frame * this.conf.count, now),
-            common.sleep_for(0), // allow Ctrl+C
-        ]))[0]);
+            if(trades.length) {
+                if(_.isUndefined(this.conf.init_price))
+                    this.conf.init_price = trades[0].price;
+                this.conf.price = _.last(trades).price;
+            }
+            this.emit('trades', trades);
+        }
     }
 };
 
