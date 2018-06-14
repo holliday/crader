f728dc5123d265caee4e84f61055cab27bcadc63
Doc Holliday <doc.holliday@usa.com>
2018-06-14 14:41:19 -0400

    strat/ohlcv: update
    Now the ohlcv strat can be used as a template for other candle-based
    strats.

--- strat/ohlcv.js
+++ strat/ohlcv.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const common = root_require('common');
-const ind = root_require('lib/ind'); // indicators
+root_require('core');
+const ind = root_require('lib/ind');
 root_require('lib/show');
 const table = root_require('lib/table');
 
@@ -10,28 +10,56 @@ const strat = {};
 strat.init = conf => {
     this.conf = conf;
 
+    ////////////////////
     this.table = new table();
-    this.table.add_column('Date'  , as_date , blue  );
-    this.table.add_column('Open'  , as_price        );
-    this.table.add_column('High'  , as_price, green );
-    this.table.add_column('Low'   , as_price, red   );
-    this.table.add_column('Close' , as_price, bold  );
-    this.table.add_column('Volume', as_vol  , yellow);
+    this.table.add_column('Date'  , as_date );
+    this.table.add_column('Open'  , as_price);
+    this.table.add_column('High'  , as_price);
+    this.table.add_column('Low'   , as_price);
+    this.table.add_column('Close' , as_price);
+    this.table.add_column('Volume', as_vol, yellow);
+};
+
+strat.print_line = (candle, color_date) => {
+    this.table.with('Date', color_date)
+        .with(['Open', 'High', 'Low', 'Close'], comp_to(candle.close, candle.open))
+        .print_line(candle);
 };
 
 strat.advise = trades => {
-    var ohlcv = ind.ohlcv(trades, this.conf.frame);
+    var advice;
 
-    console.log('Received', bold(ohlcv.length), 'OHLCV candles:');
+    var series = ind.ohlcv(trades, this.conf.frame);
+    if(!series.length) return;
 
-    this.table.with('*', gray).print_head();
-    ohlcv.forEach(candle =>
-        this.table.print_line(candle.timestamp,
-            candle.open, candle.high, candle.low, candle.close,
-            candle.volume,
-        )
-    );
-    console.log();
+    var candle = series.end();
+
+    // first time?
+    if(!is_def(this.timestamp)) {
+        this.timestamp = candle.timestamp;
+
+        // print head & preroll candles
+        this.table.with('*', white).print_head();
+        series.forEach(candle => strat.print_line(candle, gray));
+    }
+
+    move_prev();
+    erase_end();
+
+    // new candle
+    if(this.timestamp !== candle.timestamp) {
+        this.timestamp = candle.timestamp;
+
+        // print prior candle
+        strat.print_line(series.end(-1), blue);
+    }
+
+    // print current candle
+    candle.timestamp = trades.end().timestamp;
+    strat.print_line(candle, bg_blue);
+
+    return advice;
 }
 
+////////////////////
 module.exports = strat;
