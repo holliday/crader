a9bc519367a63d1f5c5c29e9d8067d250db47578
Doc Holliday <doc.holliday@usa.com>
2018-06-14 23:26:13 -0400

    strat/macd: update

--- strat/macd.js
+++ strat/macd.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const _ = require('underscore');
-
 const advice = root_require('lib/advice');
 const common = root_require('common');
 root_require('core');
-const ind = root_require('lib/ind'); // indicators
+const ind = root_require('lib/ind');
 root_require('lib/show');
 const table = root_require('lib/table');
 const trend = root_require('lib/trend');
@@ -35,68 +33,62 @@ strat.init = conf => {
     this.table.add_column('Low'   , as_price);
     this.table.add_column('Close' , as_price);
     this.table.add_column('Volume', as_vol, yellow);
-    this.table.add_column('Hist'  , as_fixed, '+-');
+    this.table.add_column('Hist'  , as_fixed, '+');
 };
 
-strat.print_line = (candle, hist, color_date) => {
-    this.table.with( 'Date', color_date)
+strat.print_line = (candle, color_date) => {
+    this.table.with('Date', color_date)
         .with(['Open', 'High', 'Low', 'Close'], comp_to(candle.close, candle.open))
-        .with('Hist', not_in(hist, this.conf.min_down, this.conf.min_up))
-        .print_line(candle.timestamp,
-            candle.open, candle.high, candle.low, candle.close,
-            candle.volume,
-            hist
-        );
+        .with('Hist', not_in(candle.histogram, this.conf.min_down, this.conf.min_up))
+        .print_line(candle);
 };
 
 strat.advise = trades => {
     var advice;
 
-    var ohlcv = ind.ohlcv(trades, this.conf.frame);
-    if(!ohlcv.length) return;
+    var series = ind.ohlcv(trades, this.conf.frame);
+    if(!series.length) return;
 
-    var macd = ind.macd(ohlcv.map(candle => candle.close),
+    var macd = ind.macd(series.get('close'),
         this.conf.short_period, this.conf.long_period, this.conf.signal_period
     );
     if(!macd.length) return;
+    series.merge_end(macd.get(['histogram']));
 
-    var trade = _.last(trades);
-    var [ candle_done, candle_new ] = _.last(ohlcv, 2);
-    var [ macd_done, macd_new ] = _.last(macd, 2);
+    var candle = series.end();
+    var trade = trades.end();
 
     // first time?
     if(!is_def(this.timestamp)) {
-        this.timestamp = candle_new.timestamp;
+        this.timestamp = candle.timestamp;
 
-        // print head & preroll
+        // print head & preroll candles
         this.table.with('*', white).print_head();
-
-        while(macd.length < ohlcv.length) macd.unshift({ histogram: '-' });
-        ohlcv.forEach((candle, idx) =>
-            strat.print_line(candle, macd[idx].histogram, gray)
-        );
+        series.forEach(candle => strat.print_line(candle, gray));
     }
 
     move_prev();
     erase_end();
 
     // new candle
-    if(this.timestamp !== candle_new.timestamp) {
-        this.timestamp = candle_new.timestamp;
+    if(this.timestamp !== candle.timestamp) {
+        this.timestamp = candle.timestamp;
 
-        // print final line
-        strat.print_line(candle_done, macd_done.histogram, blue);
+        var done = series.end(-1);
+
+        // print prior candle
+        strat.print_line(done, blue);
 
         // MACD
-             if(macd_done.histogram >= this.conf.min_up  ) this.trend.state = 'up';
-        else if(macd_done.histogram <= this.conf.min_down) this.trend.state = 'down';
+             if(done.histogram >= this.conf.min_up  ) this.trend.state = 'up';
+        else if(done.histogram <= this.conf.min_down) this.trend.state = 'down';
 
         advice = this.trend.advise(trade.timestamp, this.conf.symbol, trade.price);
     }
 
-    // print current line
-    candle_new.timestamp = trade.timestamp;
-    strat.print_line(candle_new, macd_new.histogram, bg_blue);
+    // print current candle
+    candle.timestamp = trade.timestamp;
+    strat.print_line(candle, bg_blue);
 
     return advice;
 }
