8c008b8515092fc3b1ee8e2bab07323f4c799d72
Doc Holliday <doc.holliday@usa.com>
2018-06-15 17:40:47 -0400

    run: listen for key presses
    Ctrl + C - exit
    Ctrl + Q - exit
    Ctrl + S - print summary

--- run.js
+++ run.js
@@ -1,7 +1,8 @@
 'use strict';
 require('./core');
 
-const common = root_require('common');
+const readline = require('readline');
+const common   = root_require('common');
 
 ////////////////////
 (async () => { try {
@@ -15,13 +16,27 @@ const common = root_require('common');
     var strat  = await local_require('strat', 'base').create(conf);
     var trader = await local_require('trader', conf.trader).create(conf);
 
-    process.on('SIGINT' , () => conf.stop = true);
-    process.on('SIGTERM', () => conf.stop = true);
-
     feed.on ('trades', trades => strat.advise(trades));
     feed.on ('done'  , ()     => trader.print_summary());
     strat.on('advice', advice => trader.accept(advice));
 
+    ////////////////////
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+
+    process.stdin.on('keypress', (ch, key) => {
+        if(key.ctrl) switch(key.name) {
+            case 'c':
+            case 'q':
+                setImmediate(process.exit);
+                conf.stop = true;
+                break;
+
+            case 's': trader.print_summary(); break;
+        }
+    });
+
+    ////////////////////
     await feed.run();
 
 } catch(e) {
