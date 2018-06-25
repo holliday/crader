b2001e461df5be814f46c473b32d847831e9b7c1
Doc Holliday <doc.holliday@usa.com>
2018-06-25 00:39:52 -0400

    ReadTrader: add max buy/sell bits

--- conf/real.js
+++ conf/real.js
@@ -2,4 +2,9 @@ var conf = {};
 
 conf.trader = 'real';
 
+// maximum portion of assets to buy/sell
+// NB: do not set to more than 0.99
+conf.max_buy = 0.99;
+conf.max_sell = 0.99;
+
 module.exports = conf;
--- trader/real.js
+++ trader/real.js
@@ -12,6 +12,16 @@ class RealTrader extends TraderBase {
     static async create(conf) {
         console.log('Creating', as.bold('real'), 'trader');
 
+        conf.max_buy = parse.float(conf, 'max_buy');
+        if(isNaN(conf.max_buy)) conf.max_buy = 0.99;
+
+        conf.max_sell = parse.float(conf, 'max_sell');
+        if(isNaN(conf.max_sell)) conf.max_sell = 0.99;
+
+        console.log('Trading settings:');
+        console.log('  buy' , as.bold(as.pct(conf.max_buy , '-')));
+        console.log('  sell', as.bold(as.pct(conf.max_sell, '-')));
+
         return new RealTrader(conf);
     }
 
