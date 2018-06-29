b3ccccc01eb4e388fff5e62dc2d2f18c1972257a
Doc Holliday <doc.holliday@usa.com>
2018-06-29 01:11:52 -0400

    strat/trades: show time

--- strat/trades.js
+++ strat/trades.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const ansi  = lib_require('ansi');
 const as    = lib_require('as');
 const is    = lib_require('is');
 const Table = lib_require('table');
@@ -13,25 +14,30 @@ strat.init = conf => {
     this.table.add_column('Date'  , as.date, as.blue);
     this.table.add_column('Price' , as.price);
     this.table.add_column('Amount', as.vol, as.yellow);
+
+    this.prev_trade = {};
 };
 
 strat.advise = trades => {
-    if(trades.length === 0) return;
+    if(trades.length > 0
+        && trades.end().timestamp !== this.prev_trade.timestamp) {
+    //
+        if(is.undef(this.prev_trade.timestamp))
+            this.table.with('*', as.white).print_head();
 
-    if(is.undef(this.trade)) {
-        this.trade = { timestamp: 0 };
-        this.table.with('*', as.white).print_head();
+        trades.forEach(trade => {
+            if(trade.timestamp <= this.prev_trade.timestamp) return;
+
+            this.table.with('Price',
+                as.comp_to(this.prev_trade.price, trade.price)
+            ).print_line(trade);
+
+            this.prev_trade = trade;
+        });
     }
 
-    trades.forEach(trade => {
-        if(trade.timestamp <= this.trade.timestamp) return;
-
-        this.table.with('Price',
-            as.comp_to(this.trade.price, trade.price)
-        ).print_line(trade);
-
-        this.trade = trade;
-    });
+    console.log(as.now());
+    ansi.move_prev();
 }
 
 module.exports = strat;
